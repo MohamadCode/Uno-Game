b840ddef27afb8211f92ef12aa165def59b4e6e7
MohamadCode <m-munla2004@hotmail.com>
2023-06-16 04:28:14 +0300

    conditions

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-let firstCard = 9
-let secondCard = 6
+// range from 0-9
+
+let firstCard = 2
+let secondCard = 0
 let sum = firstCard + secondCard
+
+
+if ( sum === 9 ) {
+    console.log("Just Started the Game 💭")
+} else if ( sum < 9)  { 
+    console.log("Playing & Drawing the cards 🧐")
+} else if ( sum === 2 ) {
+    console.log("Don't forget to say Uno before Drawing the last card ")
+} else ( sum === 1); {
+    console.log("Uno !")
+}
